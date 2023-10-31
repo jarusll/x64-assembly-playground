c6834e928c7267251e77b885b89121ef917a8071
Suraj Yadav <ysuraj877@gmail.com>
2023-10-31 19:42:21 +0000

    Format

--- read/main.asm
+++ read/main.asm
@@ -4,16 +4,16 @@ section .bss
 section .text
 	global main
 main:
-	mov rax, 0
-	mov rdi, 0
-	mov rsi, input
-	mov rdx, 10
-	syscall ; input call
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, input
-	mov rdx, 10
-	syscall ; write call
-	mov rax, 60
-	mov rdi, 0
+	mov     rax, 0     ; read
+	mov     rdi, 0
+	mov     rsi, input
+	mov     rdx, 10
+	syscall            ; input call
+	mov     rax, 1     ; write
+	mov     rdi, 1
+	mov     rsi, input
+	mov     rdx, 10
+	syscall            ; write call
+	mov     rax, 60
+	mov     rdi, 0
 	syscall
--- write/main.asm
+++ write/main.asm
@@ -4,11 +4,11 @@ section .bss
 section .text
 	global main
 main:
-	mov rax, 1
+	mov rax, 1       ; write
 	mov rdi, 1
 	mov rsi, message
 	mov rdx, 14
 	syscall
-	mov rax, 60
+	mov rax, 60      ; exit
 	mov rdi, 0
 	syscall
